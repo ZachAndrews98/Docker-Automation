4f92aea1d253c8125d7ea6046b33040ddb31ee77
Zach <andrewsz@allegheny.edu>
2020-01-29 23:28:59 -0500

    Linux install and setting up travis

---
+++ install_docker.py
@@ -0,0 +1,58 @@
+""" Checks OS and installs Docker following a specific instruction set """
+import os
+import platform
+import subprocess
+
+
+PLATFORM = platform.system()
+
+
+def main():
+    """ Main installation function """
+    if PLATFORM == "Linux":
+        # pylint: disable=W1505
+        distro = platform.linux_distribution()
+    file = get_instructions(distro)
+    if file != "No Instruction Set":
+        execute_instructions(file)
+    if not confirm_installation():
+        print("Docker successfully installed")
+    else:
+        print("Issue installing Docker")
+
+
+def get_instructions(distro):
+    """ Determines what instruction set is required to install Docker """
+    file = ""
+    if distro[0] == "Ubuntu":
+        file = "./instructions/ubuntu"
+    elif distro[0] == "CentOS":
+        file = "./instructions/centos"
+    elif distro[0] == "Debian":
+        file = "./instructions/debian"
+    elif distro[0] == "Fedora":
+        file = "./instructions/fedora"
+    else:
+        file = "No Instruction Set"
+    return file
+
+
+def execute_instructions(file):
+    """ Executes the installation instructions """
+    commands = open(file, 'r')
+    for command in commands:
+        command = command.replace("\n", "")
+        print(command)
+        os.system(command)
+
+
+def confirm_installation():
+    """ Confirms if Docker was properly installed """
+    proc = subprocess.Popen(
+        ["docker", "run", "hello-world"], stdout=subprocess.PIPE)
+    # pylint: disable=W0106
+    proc.communicate()[0]
+    return proc.returncode
+
+
+main()
