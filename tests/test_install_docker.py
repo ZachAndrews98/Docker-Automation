577e4a579281304c8f2dda341ed4bb4ad74cf245
zach <andrewsz@allegheny.edu>
2020-02-05 19:21:43 -0500

    Attempt to add install test

--- tests/test_install_docker.py
+++ tests/test_install_docker.py
@@ -6,11 +6,12 @@ import io
 from auto import install_docker
 
 
-# def test_install(monkeypatch):
-#     """ Test Install Method """
-#     monkeypatch.setattr('PLATFORM', io.StringIO('Linux'))
-#     monkeypatch.setattr('distro', io.StringIO(("Ubuntu", "", "")))
-#     assert install_docker.install() == "Docker successfully installed"
+def test_install():
+    """ Test Install Method """
+    # monkeypatch.setattr('PLATFORM', io.StringIO('Linux'))
+    # monkeypatch.setattr('distro', io.StringIO(("Ubuntu", "", "")))
+    assert install_docker.install() == "Docker already installed"
+    
 
 def test_get_instructions():
     """ Test Correct Instruction Set Found """
