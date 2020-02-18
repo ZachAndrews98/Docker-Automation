8af120b131250e6ba56d6bf1eab7f2c82fdf79af
Zach <andrewsz@allegheny.edu>
2020-02-18 13:24:40 -0500

    Attempts to get working terminal

--- static/js/terminal.js
+++ static/js/terminal.js
@@ -1,4 +1,12 @@
-var term = new Terminal();
+// var term = new Terminal();
+// term.open(document.getElementById('terminal'));
+// term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
+// term.prompt();
+import { FitAddon } from 'xterm-addon-fit';
+
+const term = new Terminal();
+const fitAddon = new FitAddon();
+term.loadAddon(fitAddon);
 term.open(document.getElementById('terminal'));
-term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
+term.write('$ ')
 term.prompt();
