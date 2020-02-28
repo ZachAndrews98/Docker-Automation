07bf39759b4aa1ae132ede681ce105851636e28e
Zach <andrewsz@allegheny.edu>
2020-02-27 23:58:33 -0500

    Fixed test

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -10,17 +10,17 @@ from auto import generate
 LOCATION = [
     ('./gentest', {'py', 'c', 'java', 'js', 'sh', 'rb', 'go'})
 ]
-@pytest.mark.parametrize("location, expected", LOCAITON)
+@pytest.mark.parametrize("location, expected", LOCATION)
 def test_get_file_types(location, expected):
     """ Checks that correct file types are found """
     assert generate.get_file_types(location) == expected
 
 
-LOCAITON = [
+LOCATION = [
     ('./gentest', './gentest/Dockerfile', True),
     ('./gentest', './instructions/Dockerfile', False)
 ]
-@pytest.mark.parametrize("location, path_check, expected", LOCAITON)
+@pytest.mark.parametrize("location, path_check, expected", LOCATION)
 def test_generate_dockerfile(location, path_check, expected):
     """ Checks if the proper Dockerfile is created """
     generate.generate_dockerfile(location)
