7a3d5aad75915af59f3ab222f134ec5839e3a27d
Zachary Andrews <andrewsz@allegheny.edu>
2020-05-05 17:40:01 -0400

    Changes to gitignore, evaluations for installation

--- eval.py
+++ eval.py
@@ -10,16 +10,17 @@ from evaluator import data
 from evaluator import evaluate_build as build
 from evaluator import evaluate_run as run
 from evaluator import evaluate_generate as generate
+from evaluator import evaluate_install as install
 
-print("Pulling Required Images")
-images.pull_image("debian", tag="bullseye")
-images.pull_image("hello-world")
+# print("Pulling Required Images")
+# images.pull_image("debian", tag="bullseye")
+# images.pull_image("hello-world")
 # build.evaluate_build_image(10)
 # run.evalutate_run_hello_world(10)
-build.threaded_evaluate_build_image((1,5,10,15))
+# build.threaded_evaluate_build_image((1,5,10,15))
 # run.threaded_evaluate_run_hello_world((1,5,10,15))
 # generate.evaluate_generate()
-
+print(install.evaluate_install())
 
 # print("Build Image Test")
 # print("\tTool Run Times: " + str(data.TOOL_DATA['build_image_times']))
@@ -35,19 +36,19 @@ build.threaded_evaluate_build_image((1,5,10,15))
 # print("\tAverage Terminal: " + str(data.TERM_DATA['hello_world_ave']))
 # print("\n\n")
 
-print("Threaded Build Image Test")
-print(
-    "\tNumber of Threads List: " + str(
-        data.TOOL_DATA["thread_build_image_threads"]
-    )
-)
-print("\tTool Run Times: " + str(data.TOOL_DATA["thread_build_image_times"]))
-print(
-    "\tTerminal Run Times: " + str(data.TERM_DATA["thread_build_image_times"])
-)
-print("\tAverage Tool: " + str(data.TOOL_DATA['thread_build_image_ave']))
-print("\tAverage Terminal: " + str(data.TERM_DATA['thread_build_image_ave']))
-print("\n\n")
+# print("Threaded Build Image Test")
+# print(
+#     "\tNumber of Threads List: " + str(
+#         data.TOOL_DATA["thread_build_image_threads"]
+#     )
+# )
+# print("\tTool Run Times: " + str(data.TOOL_DATA["thread_build_image_times"]))
+# print(
+#     "\tTerminal Run Times: " + str(data.TERM_DATA["thread_build_image_times"])
+# )
+# print("\tAverage Tool: " + str(data.TOOL_DATA['thread_build_image_ave']))
+# print("\tAverage Terminal: " + str(data.TERM_DATA['thread_build_image_ave']))
+# print("\n\n")
 
 # print("Threaded Hello World Test")
 # print(
--- evaluator/evaluate_install.py
+++ evaluator/evaluate_install.py
@@ -1,1 +1,65 @@
 """ Evaluate Docker Installation """
+
+import os
+
+from auto import images, containers
+
+
+def evaluate_ubuntu():
+    """ Evaluates installation process on Ubuntu """
+    images.delete_image("ubuntest:latest".split(','))
+    containers.delete_container("ubuntest".split(','))
+    images.build_image(
+        "/Docker-Automation/evaluator/Dockerfiles/Ubuntest",
+        image_name="ubuntest",
+        threaded=False
+    )
+    return images.run_image(
+        "ubuntest:latest",
+        args="-v /var/run/docker.sock:/var/run/docker.sock -it --name ubuntest",
+        sep=False
+    )
+
+
+def evaluate_fedora():
+    """ Evaluates installation process on Fedora """
+    images.delete_image("fedortest:latest".split(','))
+    containers.delete_container("fedortest".split(','))
+    images.build_image(
+        "/Docker-Automation/evaluator/Dockerfiles/Fedortest",
+        image_name="fedortest",
+        threaded=False
+    )
+    return images.run_image(
+        "fedortest:latest",
+        args="-v /var/run/docker.sock:/var/run/docker.sock -it --name fedortest",
+        sep=False
+    )
+
+
+def evaluate_debian():
+    """ Evaluates installation process on Debian """
+    images.delete_image("debtest:latest".split(','))
+    containers.delete_container("debtest".split(','))
+    images.build_image(
+        "/Docker-Automation/evaluator/Dockerfiles/Debtest",
+        image_name="debtest",
+        threaded=False
+    )
+    return images.run_image(
+        "debtest:latest",
+        args="-v /var/run/docker.sock:/var/run/docker.sock -it --name debtest",
+        sep=False
+    )
+
+
+def evaluate_install():
+    ubuntu = evaluate_ubuntu()
+    print(ubuntu)
+    # fedora = evaluate_fedora() == None
+    # debian = evaluate_debian() == None
+
+    print("Evaluating Installation Process")
+    print("\tUbuntu: " + str(ubuntu))
+    print("\tFedora: " + str(fedora))
+    print("\tDebian: " + str(debian))
